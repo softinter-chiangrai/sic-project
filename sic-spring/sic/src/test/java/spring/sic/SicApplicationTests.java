fb09b8f38f3f1087b1cbc35ac320bba4e4d7873f
Phurin Sukman <phurinsukman1@gmail.com>
2026-05-19 11:38:49 +0700

    first

---
+++ sic-spring/sic/src/test/java/spring/sic/SicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
