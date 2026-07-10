859824c2a5714a9d180ceb2270be95a0d44be8c8
Phurin <phurinsukman1@gmail.com>
2026-07-10 17:45:40 +0700

    เเก่ isactive กับburt06

--- sic-app/src/app/feature/bu/rt/burt06/burt06.service.ts
+++ sic-app/src/app/feature/bu/rt/burt06/burt06.service.ts
@@ -11,7 +11,7 @@ export interface ApprovalFlow {
   documentType: string;
   approvalMode: string;
   description?: string;
-  isActive: boolean;
+  active: boolean;
   steps: ApprovalFlowStep[];
   rowVersion?: number;
 }
